5506ff84f3cbdd677405e22dcc90d2c697c6550c
ryantando <ryantando@gmail.com>
2020-11-21 13:06:43 +0700

    Add Icon in Home

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {View, Text, Dimensions, StyleSheet, FlatList} from 'react-native';
+import * as Animatable from 'react-native-animatable';
 import {
   colorGreen1,
   colorGreen2,
@@ -12,11 +13,20 @@ import {
   colorGreen4,
 } from '../utils/colors';
 import ButtonNativeFeedBack from '../components/Buttons';
+import PlusIcon from '../svg/PlusIcon';
+import CancelIcon from '../svg/CancelIcon';
+import ClockIcon from '../svg/ClockIcon';
+import ZeroIcon from '../svg/ZeroIcon';
 
 const {width, height} = Dimensions.get('window');
 
 const styles = StyleSheet.create({
-  container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#000',
+  },
   buttonContainer: {
     marginTop: height * 0.04,
   },
@@ -42,6 +52,7 @@ const styles = StyleSheet.create({
     borderBottomRightRadius: 15,
   },
   text: {fontSize: 25, color: colorWhite, textAlign: 'center'},
+  icon: {position: 'absolute', left: width * 0.07, top: height * 0.03},
 });
 
 const buttonList = [
@@ -50,24 +61,28 @@ const buttonList = [
     navigateTo: 'Sum',
     bgLeft: colorGreen1,
     bgRight: colorGreen2,
+    icon: <PlusIcon />,
   },
   {
     text: 'Multiply X & Y',
     navigateTo: 'Multiply',
     bgLeft: colorBlue1,
     bgRight: colorBlue2,
+    icon: <CancelIcon />,
   },
   {
     text: 'First N\nPrime Number',
     navigateTo: 'FirstNPrime',
     bgLeft: colorSoftBlue1,
     bgRight: colorSoftBlue2,
+    icon: <ClockIcon />,
   },
   {
     text: 'First N\nFibonacci Number',
     navigateTo: 'FirstNFibonacci',
     bgLeft: colorGreen3,
     bgRight: colorGreen4,
+    icon: <ZeroIcon />,
   },
 ];
 
@@ -91,6 +106,14 @@ function Home(props) {
               ...styles.buttonContainerContent,
               backgroundColor: item.bgLeft,
             }}>
+            <View style={styles.icon}>
+              <Animatable.View
+                animation="swing"
+                easing="ease-out"
+                iterationCount="infinite">
+                {item.icon}
+              </Animatable.View>
+            </View>
             <View
               style={{
                 ...styles.buttonRight,
